481b8520701dcc351039ef8ed9fd9e83e9697524
Emilien Demartin <antoine.gilliard@hotmail.fr>
2026-08-07 23:55:30 +0200

    D1 settings

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,7 @@ import { defineConfig } from "vite";
 import hostingConfig from "./.openai/hosting.json";
 import { sites } from "./build/sites-vite-plugin";
 
-const SITE_CREATOR_PLACEHOLDER_DATABASE_ID =
-  "00000000-0000-4000-8000-000000000000";
+const LOVE_O_MATIC_DATABASE_ID = "9cff53be-4e0a-435d-89d0-57b445eb53a8";
 
 const { d1, r2 } = hostingConfig;
 
@@ -18,8 +17,8 @@ const localBindingConfig = {
     ? [
         {
           binding: d1,
-          database_name: "site-creator-d1",
-          database_id: SITE_CREATOR_PLACEHOLDER_DATABASE_ID,
+          database_name: "love-o-matic",
+          database_id: LOVE_O_MATIC_DATABASE_ID,
         },
       ]
     : [],
